e56fc1fe69ac5a0db4f1ea0d96387f335e4875d7
Pablo Ventura <pablogventura@gmail.com>
2016-08-17 21:17:12 -0300

    comentarios

--- main.c
+++ main.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <pthread.h>
- 
+
 int numero = 0;
- 
+
 void zig();
 void zag();
- 
+
 int main(void)
 {
- pthread_t hilo0, hilo1;
- 
- pthread_create(&hilo0, NULL, (void *) &zig, NULL); 
- pthread_create(&hilo1, NULL, (void *) &zag, NULL); 
-  
- pthread_join(hilo0, NULL);
- pthread_join(hilo1, NULL);
- numero = numero % 100;
- printf("%i\n",numero);
- return 0;
-}
- 
-void zig() {
- int i=0;
- for(i=0; i < 10000000; i++){
-     numero = (numero+1) % 100;
- }
- return;
+	pthread_t hilo0, hilo1;
+
+	// pongo a ejecutar los hilos
+	pthread_create(&hilo0, NULL, (void *)&zig, NULL);
+	pthread_create(&hilo1, NULL, (void *)&zag, NULL);
+
+	// espero a que terminen
+	pthread_join(hilo0, NULL);
+	pthread_join(hilo1, NULL);
+
+	// saco un modulo para que el numero quede mas lindo
+	numero = numero % 100;
+	printf("%i\n", numero);
+	return 0;
 }
 
-void zag() {
- int i=0;
- for(i=0; i < 10000000; i++){
-     numero = (numero*numero) % 100;
- }
- return;
+void zig()
+{
+	// suma 1 10 millones de veces
+	int i = 0;
+	for (i = 0; i < 10000000; i++)
+	{
+		numero = (numero + 1) % 100;
+	}
+	return;
+}
+
+void zag()
+{
+	// multiplica por si mismo 10 millones de veces
+	int i = 0;
+	for (i = 0; i < 10000000; i++)
+	{
+		numero = (numero * numero) % 100;
+	}
+	return;
 }
